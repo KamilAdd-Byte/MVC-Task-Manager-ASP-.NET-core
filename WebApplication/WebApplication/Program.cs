109b5feb4398881a01901bf843c047ff4fc17509
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-06-16 18:11:26 +0200

    Project start ready generate #1

--- WebApplication/WebApplication/Program.cs
+++ WebApplication/WebApplication/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Simple_Task_Manager_MVC
+namespace WebApplication
 {
     public class Program
     {
--- WebApplication/WebApplication/Startup.cs
+++ WebApplication/WebApplication/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Simple_Task_Manager_MVC
+namespace WebApplication
 {
     public class Startup
     {
